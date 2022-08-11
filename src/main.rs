2132add1fadaf366579b0361be08fc8c79e8b97c
KrunchyKrisp <m.damidavicius@gmail.com>
2022-08-11 23:43:57 +0300

    Fixes

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::time::Instant;
+use num_format::{Locale, ToFormattedString};
+use hhmmss::Hhmmss;
 
 fn main() {
-    //let starting = Some((Vec::from([[0, 1], [2, 3], [4, 5], [0, 1], [4, 5], [0, 2], [0, 5], [3, 5], [1, 4], [1, 3], [0, 5], [2, 3]]), 1380));
-    let starting = None;
-    network_generator::<7>(16, true, starting);
+    let starting = Some((Vec::from(
+        [[0, 1], [2, 3], [4, 5], [6, 7], [0, 2], [4, 6], [0, 4], [1, 3], [1, 2], [1, 3], [4, 5], [2, 5], [2, 6], [4, 6], [1, 2], [1, 3], [1, 2], [1, 3], [1, 2]]
+    ), 8812));
+    // let starting = None;
+    // network_generator::<6>(12, true, starting);
+    network_generator(8, 19, true, starting);
 }
 
-fn execute_pair<const N: usize>(data: &mut [usize; N], left: usize, right: usize) {
+fn execute_pair(data: &mut Vec<usize>, left: usize, right: usize) {
     if data[left] > data[right] {
         let temp = data[left];
         data[left] = data[right];
@@ -14,58 +19,62 @@ fn execute_pair<const N: usize>(data: &mut [usize; N], left: usize, right: usize
     }
 }
 
-fn increase<const N: usize>(network: &mut Vec<[usize; 2]>, pos: usize) {
+fn increase(network: &mut Vec<[usize; 2]>, n: usize, pos: usize, offset: usize) {
     network[pos][1] += 1;
-    if network[pos][1] > N - 1 {
+    if network[pos][1] > n - 1 {
         network[pos][0] += 1;
         network[pos][1] = network[pos][0] + 1;
-        if network[pos][0] > N - 1 || network[pos][1] > N - 1 {
-            network[pos][0] = 0;
-            network[pos][1] = 1;
+        if network[pos][0] > n - 1 || network[pos][1] > n - 1 {
+            network[pos][0] = offset;
+            network[pos][1] = offset + 1;
             if pos > 0 {
-                increase::<N>(network, pos - 1);
+                increase(network, n, pos - 1, offset);
             }
         }
     }
     if pos > 0 && network[pos] == network[pos - 1] {
-        increase::<N>(network, pos);
+        increase(network, n, pos, offset);
     }
 }
 
-fn execute_network<const N: usize>(network: & Vec<[usize; 2]>, data: &mut [usize; N]) {
+fn execute_network(network: & Vec<[usize; 2]>, data: &mut Vec<usize>) {
     for pair in network.iter() {
         execute_pair(data, pair[0], pair[1]);
     }
 }
 
-fn network_tester<const N: usize>(network: & Vec<[usize; 2]>) -> bool {
-    let (mut data, data_end, mut data_temp): ([usize; N], [usize; N], [usize; N]) = ([0;N], [0;N], [0;N]);
+fn network_tester(network: & Vec<[usize; 2]>, n: usize, data: &mut Vec<usize>, data_temp: &mut Vec<usize>) -> bool {
+    data.fill(0);
     loop {
-        data_temp.clone_from_slice(&data);
-        execute_network(& network, &mut data_temp);
-        for i in 0..N-1 {
+        data_temp.copy_from_slice(&data);
+        execute_network(& network, data_temp);
+        for i in 0..n-1 {
             if data_temp[i] > data_temp[i+1] {
                 return false;
             }
         }
-        for i in (0..N).rev() {
+        for i in (0..n).rev() {
             if data[i] == 0 {
                 data[i] = 1;
                 break;
             }
             data[i] = 0;
         }
-        if data == data_end {
+        if data.iter().sum::<usize>() == 0 {
             break;
         }
     }
     return true;
 }
 
-fn network_generator<const N: usize>(mut depth: usize, pair_wise: bool, starting: Option<(Vec<[usize; 2]>, usize)>) -> Vec<[usize; 2]> {
+fn network_generator(n: usize, mut depth: usize, pair_wise: bool, starting: Option<(Vec<[usize; 2]>, usize)>) -> Vec<[usize; 2]> {
     let now = Instant::now();
-    let (mut network, mut end);
+    let mut network = Vec::new();
+    let mut end;
     let (mut counter, mut m_counter): (usize, usize) = (0, 0);
+    let offset: usize = if pair_wise {1} else {0};
+    let mut generate: bool;
+    let (mut data, mut data_temp): (Vec<usize>, Vec<usize>) = (vec![0;n], vec![0;n]);
 
     loop {
         println!("Testing Depth: {}", depth);
@@ -74,20 +83,24 @@ fn network_generator<const N: usize>(mut depth: usize, pair_wise: bool, starting
         let mut k: usize = 0;
         for i in 0..depth {
             k = (k % 2) + 1;
-            end[i][0] = 0;
-            end[i][1] = k;
+            end[i][0] = offset;
+            end[i][1] = offset + k;
         }
-        
+        generate = true;
         if let Some(ref x) = starting {
-            network = x.0.to_vec();
-            m_counter = x.1;
-        } else {
+            if x.0.len() == depth {
+                network = x.0.to_vec();
+                m_counter = x.1;
+                generate = false;
+            }
+        }
+        if generate {
             network = vec![[0; 2]; depth];
             k = 0;
             let (mut i, mut j): (usize, usize) = (0, 1);
             if pair_wise {
-                while i < depth && j < N {
-                    while i < depth && k < N - j {
+                while i < depth && j < n {
+                    while i < depth && k < n - j {
                         
                         network[i][0] = k;
                         network[i][1] = k + j;
@@ -96,19 +109,14 @@ fn network_generator<const N: usize>(mut depth: usize, pair_wise: bool, starting
                         k += 2 * j;
                     }
                     j *= 2;
+                    k = 0;
                 }
-                j = 2;
-                while i < depth && j < N {
-                    network[i][0] = 0;
-                    network[i][1] = j;
-                    j *= 2;
-                    i += 1;
-                }
-            }
+            } 
+            k = 1;
             while i < depth {
                 k = (k % 2) + 1;
-                network[i][0] = 0;
-                network[i][1] = k;
+                network[i][0] = offset;
+                network[i][1] = offset + k;
                 i += 1
             }
         }
@@ -117,20 +125,20 @@ fn network_generator<const N: usize>(mut depth: usize, pair_wise: bool, starting
         loop {
             if counter == 100_000_000 {
                 m_counter += 1;
-                println!("Testing {}00Mth: {:?}", m_counter, network);
-                println!("Total Time: {:?}", now.elapsed());
+                println!("Testing {} Mth: {:?}", (m_counter*100).to_formatted_string(&Locale::en), network);
+                println!("Total Time: {}", now.elapsed().hhmmssxxx());
                 counter = 0;
             }
             counter += 1;
             
-            if network_tester::<N>(& network) {
-                println!("Network Found For N = {}: {:?}", N, network);
+            if network_tester(& network, n, &mut data, &mut data_temp) {
+                println!("Network Found For n = {}: {:?}", n, network);
                 println!("At depth {}", depth);
-                println!("At Iteration {}00_{:0>6}", m_counter, counter);
-                println!("Total Time: {:?}", now.elapsed());
+                println!("At Iteration {}", (m_counter*100_000_000 + counter).to_formatted_string(&Locale::en));
+                println!("Total Time: {}", now.elapsed().hhmmssxxx());
                 return network;
             }
-            increase::<N>(&mut network, depth - 1);
+            increase(&mut network, n, depth - 1, offset);
 
             if network == end {
                 break;
